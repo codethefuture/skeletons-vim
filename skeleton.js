d6a86a530461b0cfc9a8f905fc0e7e4ccdb7295f
codethefuture <com.codethefuture@gmail.com>
2013-01-02 19:44:15 +0900

    css, html, js だけ取りあえず適当に定義

---
+++ skeleton.js
@@ -0,0 +1,20 @@
+;(function() {
+  // add variables start
+  var root = this;
+  var document = window.document;
+
+  // Version
+  var AAAAAA;
+  AAAAAA.VERSION = '0.0.0';
+  // add variables end
+
+  // test aaaaa method
+  function aaaaa() {
+  }
+
+  // test bbbbb method
+  function bbbbb() {
+  }
+
+}());
+
